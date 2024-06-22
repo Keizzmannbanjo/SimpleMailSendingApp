048fbacec9bf65019be1268a5494640f784bb4bc
KeizzmannBanjo <keizzmannbanjo@gmail.com>
2024-06-22 06:50:05 +0100

    Add project files.

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,31 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+
+namespace SimpleMailSendingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult SendEmail(string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("deangelo.sanford68@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse("deangelo.sanford68@ethereal.email"));
+            email.Subject = "Test Email";
+            email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("deangelo.sanford68@ethereal.email", "gducAqXFdEJj5FB4RP");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+            return Ok("Message Sent");
+        }
+    }
+}
